f233aa04f5e9c7976641930c67f56b431bec374b
crosscay <caicedocarlos0306@gmail.com>
2020-09-20 17:15:06 -0500

    mostrando la letra de la cancion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Fragment, useState, useEffect  } from 'react';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
 import axios from 'axios';
 
 function App() {
 
   // definir el state
   const [ busquedaLetra, guardarBusquedaLetra ] = useState({});
-  
-  const [ letra, guardarLetra ] = useState(''); 
+   
+  const [ letra, guardarLetra] = useState('');
 
   useEffect(() => {
-    if (Object.keys(busquedaLetra).length === 0) return;
-    console.log('no se ejecuta ');
+    if(Object.keys(busquedaLetra).length === 0 ) return;
+
     const consultarApiLetra = async () => {
       const { artista, cancion } = busquedaLetra;
-
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-
+      // const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
       const resultado = await axios(url);
-      
+      // const [letra, informacion] = await Promise.all([
+      //   axios(url)
+      //   // axios(url2)
+      // ]);
+
+      // guardarLetra(letra.data.lyrics);
+      // guardarInfo(informacion.data.artists[0]);
+
       guardarLetra(resultado.data.lyrics);
-    } 
+    }
     consultarApiLetra();
   }, [busquedaLetra]);
 
@@ -29,6 +36,18 @@ function App() {
       <Formulario 
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            <Cancion 
+              letra={letra}              
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
